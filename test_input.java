a0b8c459c9e7f91d6322e56c2573589614c1973a
Nishanth-8Git <154577538+Nishanth-8Git@users.noreply.github.com>
2024-01-08 20:44:29 +0530

    Uploading the Second Java Program
    Uhh Hi there! this is to simply test whether I understood the input function properly or not!

---
+++ test_input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test_input
+{
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Seeing if this accepts inputs:");
+        String test = in.next();
+        System.out.println(test);
+    }
+}
