ecb0c8c77f14907471492d74e7ab5fc4590d6c5b
Nishanth-8Git <154577538+Nishanth-8Git@users.noreply.github.com>
2024-02-07 20:27:37 +0530

    This is another regularly scheduled update.

--- JAVA_Test_Field/insane_msg_passing.java
+++ JAVA_Test_Field/insane_msg_passing.java
@@ -1,14 +1,18 @@
 ///////////////////////////Declaring Multiple Classes to test functions. ~Completed Successfully.~/////////////
 
 /***ctrl+alt (+shift[for caps] => ṭēśṭīṇg) */
+
 class scenario{
+
     int a=10;
     int b=10;
     int c=10;
     double spl = (int) 3.1467790 + 8.8846 ; //testing the conversion!
+
     void random_one(){
         System.out.println(spl+"\n");
     }
+    
 } 
 
 class scenario2{
@@ -26,13 +30,18 @@ class scenario2{
 
 ////////////////////////////////This is a class inheritance test area ~Completed Successfullly.~/////
 class sup1{
+
     protected static int a=100;
+
 } 
 
 class sub1 extends sup1{
-    static void random_three(){
+     int random_three(){   //earlier was static random int three
+
         System.out.println(a);
         System.out.println(a+"\n\n\n"); //protected member(s) can be accessed by the subclass(es).
+        return a;
+
     }
 }
 /////////////////////////////////////////////~Test Completed Successfully.~/////////////////////////
@@ -40,32 +49,45 @@ class sub1 extends sup1{
 
 /////////////////////////////////Testing abstract classes in java with some huge method enforcements//////////////
 abstract class actual{
+
     ///Making the sub-classes to implement these inside them (forcefully using abstract classes.)
    abstract int must_method1();
    abstract double must_method2();
    abstract void must_method3();
-   abstract void must_method4();
+   abstract char must_method4();
 
 }
 
 class trial1 extends actual{
+
     int must_method1(){
+
         int ret;
         ret =1;
         return ret;
+
     }
 
     double must_method2(){
+
         double retu;
         retu=1.33;
         return retu;
+
     }
 
     void must_method3(){
 
+        String test = "This";
+        System.out.println(test);
+
     }
 
-    void must_method4(){
+    char must_method4(){
+
+        char test;
+        test = 'X';
+        return test;
 
     }
 }
@@ -74,14 +96,18 @@ class trial1 extends actual{
 
 public class insane_msg_passing{
     public static void main(String args[]){
-        scenario A = new scenario();
+        scenario   A = new scenario();
         scenario2 B = new scenario2();
-        sub1     s = new sub1();
+        sub1         s = new sub1();
+        trial1        D = new trial1();
 
         //testing through methods:
 
         A.random_one();
         B.random_two();
-        s.random_three();
+        final int var  = s.random_three();
+        System.out.println("The "+var+" value  has been finalized.");
+        System.out.println(D.must_method2());
+        D.must_method3();
     }
 }
